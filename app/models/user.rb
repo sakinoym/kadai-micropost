0e2043c597df48887e4829cf86c3bc9aeaa04404
saki.y <sakino.ym@gmail.com>
2022-04-11 05:46:36 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,9 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
-
+  has_many :favorites, dependent: :destroy
+  has_many :favoritings, through: :favorites, source: :micropost
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -30,5 +32,18 @@ class User < ApplicationRecord
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
   end
+  
+  def favorite(any_micropost)
+    self.favorites.find_or_create_by(micropost_id: any_micropost.id)
+  end
+  
+  def unfavorite(any_micropost)
+    favorite = self.favorites.find_by(micropost_id: any_micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favoriting?(any_micropost)
+    self.favoritings.include?(any_micropost)
+  end
+  
 end
-
